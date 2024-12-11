56f761c72bcf473392b6131767462e4a80aa436e
joe-fourness-architect <joe@architect.co>
2024-12-11 11:59:36 -0600

    Deploy to Heroku.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import asyncio
 import json
 import secrets
+import os
+import signal
 
 from websockets.asyncio.server import broadcast, serve
 
@@ -164,8 +166,14 @@ async def handler(websocket):
         await start(websocket)
 
 async def main():
-    async with serve(handler, "", 8001):
-        await asyncio.get_running_loop().create_future() # run forever
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    port = int(os.environ.get("PORT", "8001"))
+    async with serve(handler, "", port):
+        await stop
 
 if __name__ == "__main__":
     asyncio.run(main())
